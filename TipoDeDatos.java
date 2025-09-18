a3ea0e60b36c6d345703af360efb2527e003dbca
JooseeSS <serrano.santos.jose.antonio@iescamas.es>
2025-09-18 19:22:57 +0200

    Add files via upload

---
+++ TipoDeDatos.java
@@ -0,0 +1,23 @@
+package com;
+
+public class TipoDeDatos {
+
+	public static void main(String[] args) {
+		int numero = 100;
+		float numero_decimal = 1.2F;
+		double numero_decimal2 = 1.7;
+		boolean logico=false;
+		String palabra = "Tomate";
+		char letra= 'd';
+		
+		
+		System.out.println("Numero: " +numero);
+		System.out.println("numero_decimal: " +numero_decimal);
+		System.out.println("numero_decimal2: " +numero_decimal2);
+		System.out.println("logico: " +logico);
+		System.out.println("palabra: " +palabra);
+		System.out.println("letra: " +letra);
+			
+	}
+
+}
---
+++ lectura.java
@@ -0,0 +1,12 @@
+package com;
+import java.util.*;
+public class lectura {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+System.out.println("Dime tu nombre");
+String nombre = sc.nextLine();
+System.out.println("Hola " +nombre);
+	}
+
+}
